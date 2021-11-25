6f77c1d839972b3c0ef4d89d2ffb09f4404949e5
Ernesto Guevara <neto_1208@hotmail.com>
2021-11-25 14:16:40 -0600

    dataUltimo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 import axios from "axios";
 
 function App() {
   const consultarBdd= ()=>{
     axios.get("https://7prb4nwyxa.execute-api.us-east-1.amazonaws.com/items").then(response=>{
-      console.log(response);
+      
+      const lista=response.data.Items;
+      const lastItem=lista[lista.length-1];
+      const hexaSigfox=Object.values(lastItem)[0].data;
+      console.log(hexaSigfox);
+      //lista.forEach(element => {
+        //console.log(element);
+        //for(let values in element){
+          //console.log(element[values]);
+        //}
+      //});
+      
+     
     })
   }
   
